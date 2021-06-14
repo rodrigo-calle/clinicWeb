b3f9d9aecd0f3c33c31ae6a233b4fdc69a6ce81e
rodrigo-calle <rodrigoc_0@hotmail.com>
2021-06-13 22:14:42 -0500

    project final

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 import hamburguerMenu from "./hamburguer.js";
-import secondmenu from "./submenu.js";
+// import secondmenu from "./submenu.js";
 import showZone from "./zone.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e)=>{
-    secondmenu();
+    // secondmenu();
     hamburguerMenu(".panel-btn", ".panel", ".menu a");
     showZone();
 
